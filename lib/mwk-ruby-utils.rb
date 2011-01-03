f912a7023c1e2407adddaa8a4dc69b974746a310
Matthew Krom <matthew@mwkrom.com>
2011-01-02 19:39:50 -0500

    hashify_single

--- lib/mwk-ruby-utils.rb
+++ lib/mwk-ruby-utils.rb
@@ -1,5 +1,14 @@
 class Array
-  def present_join(delim=", ")
-    self.select {|x| (x || '').strip != ""}.join(delim)
+  def present_join(in_delim=", ")
+    self.select {|x| (x || '').strip != ""}.join(in_delim)
+  end
+  
+  def hashify_single(in_sym=nil)
+    res = {}
+    self.each do |iter_el|
+      res[in_sym.nil? ? iter_el : iter_el.send(in_sym)] = iter_el
+    end
+    res
   end
 end
+
--- test/test_mwk-ruby-utils.rb
+++ test/test_mwk-ruby-utils.rb
@@ -4,4 +4,12 @@ class TestMwkRubyUtils < Test::Unit::TestCase
   should "join" do
     assert_equal ['a', nil, 'b'].present_join, "a, b"
   end
+  
+  should "hashify numbers" do
+    assert_equal [1, 2, 3].hashify_single, {1 => 1, 2 => 2, 3 => 3}
+  end
+
+  should "hashify with a sym" do
+    assert_equal [[1, 1], [2, 2], [3,3]].hashify_single(:first), {1 => [1, 1], 2 => [2, 2], 3 => [3, 3]}
+  end
 end
