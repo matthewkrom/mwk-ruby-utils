6f15093550a9e1a5cc45a7cc5a3587f82ad1383b
Matthew Krom <matthew@mwkrom.com>
2011-06-06 16:21:30 -0400

    adding blog_reader.rb from other code tree

--- Gemfile
+++ Gemfile
@@ -8,6 +8,6 @@ source "http://rubygems.org"
 group :development do
   gem "shoulda", ">= 0"
   gem "bundler", "~> 1.0.0"
-  gem "jeweler", "~> 1.5.2"
+  gem "jeweler", "~> 1.6.2"
   gem "rcov", ">= 0"
 end
---
+++ lib/mwkrom/blog_reader.rb
@@ -0,0 +1,57 @@
+module Mwkrom
+
+  ## Copyright(c) MW Krom LLC, All Rights Reserved
+  class BlogReader
+
+    attr_accessor :feed_url, :key, :expires_in
+
+    def use_expires_in
+      expires_in || 10.minutes
+    end
+
+    def articles_for(tags)
+      ActiveSupport::JSON.decode(json_articles_for(tags))
+    end
+
+    def json_articles_for(tags)
+      tkey = tags.join(":").gsub(' ', '')
+      cache_key = "blog_reader_tags:#{tkey}"
+      Rails.cache.fetch(cache_key, :expires_in => use_expires_in) do
+        json_internal_articles_for(tags)
+      end
+    end
+
+    def json_internal_articles_for(tags)
+      internal_articles_for(tags).to_json
+    end
+
+    def internal_articles_for(tags)
+      d = latest_feed_dom
+      items = (d/:item).select do |item|
+        tags.empty? || ((item/:category).any? {|x| puts x.inner_text; tags.include?(x.inner_text)})
+      end
+      items.map do |item|
+        {:url => item.at('link').inner_html,
+          :title => item.at('title').inner_html,
+          :pubDate => (item.at('pubDate') && item.at('pubDate').inner_html),
+          :creator => (item.at('dc:creator') && item.at('dc:creator').inner_html),
+          :description => item.at('description').inner_text}
+        end
+      end
+
+      def latest_feed_xml
+        cache_key = "blog_reader_all:#{key}"
+        Rails.cache.fetch(cache_key, :expires_in => use_expires_in) do
+          res = Net::HTTP.get_response(URI.parse(feed_url))
+          res && res.body
+        end
+      end
+
+      def latest_feed_dom
+        Hpricot::XML(latest_feed_xml)
+      end
+
+    end
+
+
+  end
